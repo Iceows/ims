cd07970e178e88da24ddf4d817a51e8b68c2c178
Iceows <mounierr07@gmail.com>
2022-03-29 07:52:11 +0200

    Update InstallIms.cmd

--- tools/InstallIms.cmd
+++ tools/InstallIms.cmd
@@ -2,10 +2,12 @@
 xcopy /Y "C:\GitRepo\iceows\ims\app\build\outputs\apk\debug\app-debug.apk" .
 java.exe -jar "ApkSigner.jar" sign  --key platform.pk8 --cert platform.x509.pem  --v4-signing-enabled false --out "PhhIMS.apk" "app-debug.apk"
 
-adb root
-adb remount rw, /system
-adb shell mkdir /system/priv-app/PhhIMS
-adb push PhhIMS.apk /system/priv-app/PhhIMS
-adb shell chmod 644 /system/priv-app/PhhIMS/PhhIMS.apk 
+rem adb root
+rem adb remount rw, /system
+rem adb shell mkdir /system/priv-app/PhhIMS
+rem adb push PhhIMS.apk /system/priv-app/PhhIMS
+rem adb shell chmod 644 /system/priv-app/PhhIMS/PhhIMS.apk 
 
-adb reboot
+adb install -r PhhIMS.apk 
+ 
+rem adb reboot
